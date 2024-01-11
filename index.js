da52a412744596ce9190f0e1428fb5255eb3d6b9
1stbtln <137008080+1stbtln@users.noreply.github.com>
2024-01-10 22:02:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,21 @@
 function runCode() {
-
-  var imageElement = document.querySelector('.img1');
-  function getRandomInt1(min, max) {
-    min = Math.ceil(min); // Round up the minimum value
-    max = Math.floor(max); // Round down the maximum value
+  function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  var randomInteger1 = getRandomInt1(1, 6);
+  var imageElements = document.querySelectorAll('.img1, .img2');
+  var randomInteger1 = getRandomInt(1, 6);
+  var randomInteger2 = getRandomInt(1, 6);
 
-  if (randomInteger1 === 1) {
-    imageElement.src = './dice1.png';
-  } else if (randomInteger1 === 2) {
-    imageElement.src = './dice2.png';
-  } else if (randomInteger1 === 3) {
-    imageElement.src = './dice3.png';
-  } else if (randomInteger1 === 4) {
-    imageElement.src = './dice4.png';
-  } else if (randomInteger1 === 5) {
-    imageElement.src = './dice5.png';
-  } else if (randomInteger1 === 6) {
-    imageElement.src = './dice6.png';
-  }
+  imageElements[0].src = `./dice${randomInteger1}.png`;
+  imageElements[1].src = `./dice${randomInteger2}.png`;
 
-  var imageElement = document.querySelector('.img2');
-
-  function getRandomInt2(min, max) {
-    min = Math.ceil(min); // Round up the minimum value
-    max = Math.floor(max); // Round down the maximum value
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-
-    var randomInteger2 = getRandomInt2(1, 6);
-
-  if (randomInteger2 === 1) {
-    imageElement.src = './dice1.png';
-  } else if (randomInteger2 === 2) {
-  imageElement.src = './dice2.png';
-  } else if (randomInteger2 === 3) {
-  imageElement.src = './dice3.png';
-  } else if (randomInteger2 === 4) {
-  imageElement.src = './dice4.png';
-  } else if (randomInteger2 === 5) {
-  imageElement.src = './dice5.png';
-  } else if (randomInteger2 === 6) {
-    imageElement.src = './dice6.png';
-  }
-
-if (randomInteger1 > randomInteger2) {
-  document.querySelector("h1").innerHTML = "Player One Wins!"
-} else if (randomInteger2 > randomInteger1){
-    document.querySelector("h1").innerHTML = "Player Two Wins!"
-} else {
-    document.querySelector("h1").innerHTML = "Draw"
-}
-    console.log(randomInteger1, randomInteger2);
+  var resultMessage = randomInteger1 > randomInteger2 ? "Player One Wins!" :
+                     randomInteger2 > randomInteger1 ? "Player Two Wins!" :
+                     "Draw";
+  
+  document.querySelector("h1").innerHTML = resultMessage;
+  console.log(randomInteger1, randomInteger2);
 }
 
 document.getElementById("rerunbtn").addEventListener("click", runCode);
